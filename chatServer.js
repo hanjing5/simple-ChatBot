49ce6a9d6a2d1050bd5d1fc111e90504863d6833
Han <hjing@Hans-MacBook-Pro.local>
2018-02-06 00:22:41 -0500

    mod second question

--- chatServer.js
+++ chatServer.js
@@ -1,3 +1,45 @@
+var facts = [" Some Siamese cats appear cross-eyed because the nerves from the left side of the brain go to mostly the right eye and the nerves from the right side of the brain go mostly to the left eye. This causes some double vision, which the cat tries to correct by “crossing” its eyes.[10] ",
+" Spanish-Jewish folklore recounts that Adam’s first wife, Lilith, became a black vampire cat, sucking the blood from sleeping babies. This may be the root of the superstition that a cat will smother a sleeping baby or suck out the child’s breath.[7]  ",
+" The ability of a cat to find its way home is called “psi-traveling.” Experts think cats either use the angle of the sunlight to find their way or that cats have magnetized cells in their brains that act as compasses.[10]  ",
+" The biggest wildcat today is the Siberian Tiger. It can be more than 12 feet (3.6 m) long (about the size of a small car) and weigh up to 700 pounds (317 kg).[1] ",
+" The cat who holds the record for the longest non-fatal fall is Andy. He fell from the 16th floor of an apartment building (about 200 ft/.06 km) and survived.[4]  ",
+" The claws on the cat’s back paws aren’t as sharp as the claws on the front paws because the claws in the back don’t retract and, consequently, become worn. ",
+" The earliest ancestor of the modern cat lived about 30 million years ago. Scientists called it the Proailurus, which means “first cat” in Greek. The group of animals that pet cats belong to emerged around 12 million years ago.[5] ",
+" The Egyptian Mau is probably the oldest breed of cat. In fact, the breed is so ancient that its name is the Egyptian word for “cat.”[5] ",
+" The first cartoon cat was Felix the Cat in 1919. In 1940, Tom and Jerry starred in the first theatrical cartoon “Puss Gets the Boot.” In 1981 Andrew Lloyd Weber created the musical Cats, based on T.S. Eliot’s Old Possum’s Book of Practical Cats.[7]  ",
+" The first cat in space was a French cat named Felicette (a.k.a. “Astrocat”) In 1963, France blasted the cat into outer space. Electrodes implanted in her brains sent neurological signals back to Earth. She survived the trip.[8] ",
+" The first cat show was organized in 1871 in London. Cat shows later became a worldwide craze.[7]  ",
+" The first commercially cloned pet was a cat named \"Little Nicky.\" He cost his owner $50,000, making him one of the most expensive cats ever.[10]  ",
+" The group of words associated with cat (catt, cath, chat, katze) stem from the Latin catus, meaning domestic cat, as opposed to feles, or wild cat.[3]  ",
+" The heaviest cat on record is Himmy, a Tabby from Queensland, Australia. He weighed nearly 47 pounds (21 kg). He died at the age of 10.[4]  ",
+" The largest cat breed is the Ragdoll. Male Ragdolls weigh between 12 and 20 lbs (5.4-9.0 k). Females weigh between 10 and 15 lbs (4.5-6.8 k).[4]  ",
+" The lightest cat on record is a blue point Himalayan called Tinker Toy, who weighed 1 pound, 6 ounces (616 g). Tinker Toy was 2.75 inches (7 cm) tall and 7.5 inches (19 cm) long.[4] ",
+" The little tufts of hair in a cat’s ear that help keep out dirt direct sounds into the ear, and insulate the ears are called “ear furnishings.”[5]  ",
+" The most expensive cat was an Asian Leopard cat (ALC)-Domestic Shorthair (DSH) hybrid named Zeus. Zeus, who is 90% ALC and 10% DSH, has an asking price of £100,000 ($154,000).[4]  ",
+" The most popular pedigreed cat is the Persian cat, followed by the Main Coon cat and the Siamese cat.[1]  ",
+" The most traveled cat is Hamlet, who escaped from his carrier while on a flight. He hid for seven weeks behind a panel on the airplane. By the time he was discovered, he had traveled nearly 373,000 miles (600,000 km).[4]  ",
+" The normal body temperature of a cat is between 100.5 ° and 102.5 °F. A cat is sick if its temperature goes below 100 ° or above 103 °F.[5] ",
+" The oldest cat on record was Crème Puff from Austin, Texas, who lived from 1967 to August 6, 2005, three days after her 38th birthday. A cat typically can live up to 20 years, which is equivalent to about 96 human years.[4] ",
+" The oldest cat to give birth was Kitty who, at the age of 30, gave birth to two kittens. During her life, she gave birth to 218 kittens.[4] ",
+" The richest cat is Blackie who was left £15 million by his owner, Ben Rea.[4] ",
+" The smallest feline is a masterpiece. ",
+" The smallest pedigreed cat is a Singapura, which can weigh just 4 lbs (1.8 kg), or about five large cans of cat food. The largest pedigreed cats are Maine Coon cats, which can weigh 25 lbs (11.3 kg), or nearly twice as much as an average cat weighs.[10] ",
+" The smallest wildcat today is the Black-footed cat. The females are less than 20 inches (50 cm) long and can weigh as little as 2.5 lbs (1.2 kg).[1]  ",
+" The technical term for a cat’s hairball is a “bezoar.”[7] ",
+" The term “puss” is the root of the principal word for “cat” in the Romanian term pisica and the root of secondary words in Lithuanian (puz) and Low German puus. Some scholars suggest that “puss” could be imitative of the hissing sound used to get a cat’s attention. As a slang word for the female pudenda, it could be associated with the connotation of a cat being soft, warm, and fuzzy.[11] ",
+" The tiniest cat on record is Mr. Pebbles, a 2-year-old cat that weighed 3 lbs (1.3 k) and was 6.1 inches (15.5 cm) high.[4] ",
+" The world’s rarest coffee, Kopi Luwak, comes from Indonesia where a wildcat known as the luwak lives. The cat eats coffee berries and the coffee beans inside pass through the stomach. The beans are harvested from the cat’s dung heaps and then cleaned and roasted. Kopi Luwak sells for about $500 for a 450 g (1 lb) bag.[10] ",
+" There are more than 500 million domestic cats in the world, with approximately 40 recognized breeds.[12]  ",
+" There are up to 60 million feral cats in the United States alone.[12] ",
+" Two members of the cat family are distinct from all others: the clouded leopard and the cheetah. The clouded leopard does not roar like other big cats, nor does it groom or rest like small cats. The cheetah is unique because it is a running cat; all others are leaping cats. They are leaping cats because they slowly stalk their prey and then leap on it.[1] ",
+" Unlike dogs, cats do not have a sweet tooth. Scientists believe this is due to a mutation in a key taste receptor.[5] ",
+" When a cat chases its prey, it keeps its head level. Dogs and humans bob their heads up and down.[10] ",
+" When a family cat died in ancient Egypt, family members would mourn by shaving off their eyebrows. They also held elaborate funerals during which they drank wine and beat their breasts. The cat was embalmed with a sculpted wooden mask and the tiny mummy was placed in the family tomb or in a pet cemetery with tiny mummies of mice.[5]  ",
+" While it is commonly thought that the ancient Egyptians were the first to domesticate cats, the oldest known pet cat was recently found in a 9,500-year-old grave on the Mediterranean island of Cyprus. This grave predates early Egyptian art depicting cats by 4,000 years or more.[8] ",
+" While many parts of Europe and North America consider the black cat a sign of bad luck, in Britain and Australia, black cats are considered lucky.[8] "]
+
+
+
 /*
 chatServer.js
 Author: David Goedicke (da.goedicke@gmail.com)
@@ -30,8 +72,8 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-  setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
+  socket.emit('answer',"Hey, Welcome to Cat Facts. Please wait..."); //We start with the introduction;
+  setTimeout(timedQuestion, 2500, socket,"What's your email?"); // Wait a moment and respond with a question.
 
 });
   socket.on('message', (data)=>{ // If we get a new message from the client we process it;
@@ -51,14 +93,40 @@ function bot(data,socket,questionNum) {
 
 /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
+  // answer= 'Hello ' + input + ' :-)';// output response
+  var fact = facts[Math.floor(Math.random() * facts.length)];
+  answer= 'Thank you for subscribing to Cat Facts.';// output response
   waitTime =2000;
-  question = 'How old are you?';			    	// load next question
+
+  question = "Did you know that " + fact + " Would you like to unsubscribe?";			    	// load next question
   }
   else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
-  waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
+    fact = facts[Math.floor(Math.random() * facts.length)];
+
+    if(input.toLowerCase()==='no'|| input===1){
+      answer = 'Perfect!';
+      waitTime =2000;
+      question = 'Here is another Fact: ' + fact + 'Would you like to see more Cat Facts?'; 
+    }
+    else {
+    // else if(input.toLowerCase()==='yes'|| input===0){
+        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said befor.
+        answer='What are you talking about, of course you do!'
+        // question='';
+        question = 'Here is another Fact: ' + fact + 'Would you like to see more Cat Facts?'; 
+        waitTime =0;
+        questionNum--; // Here we go back in the question number this can end up in a loop
+    }
+    // else{
+    //   answer=' I did not understand you. Can you please answer with simply with yes or no.'
+    //   question='';
+    //   questionNum--;
+    //   waitTime =0;
+    // }
+
+  // answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
+  // waitTime =2000;
+  // question = 'Where do you live?';			    	// load next question
   }
   else if (questionNum == 2) {
   answer= ' Cool! I have never been to ' + input+'.';
@@ -92,7 +160,7 @@ function bot(data,socket,questionNum) {
   // load next question
   }
   else{
-    answer= 'I have nothing more to say!';// output response
+    answer= 'Thank you for using Cat Fact! We will be sending your Cat Facts frequently!';// output response
     waitTime =0;
     question = '';
   }
